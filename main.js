7305cd683fcab853da4a143d7e2e90b89d0c9957
i-amitabh <amitabh2002m@gmail.com>
2025-04-16 15:21:15 +0530

    add player's state

--- main.js
+++ main.js
@@ -13,11 +13,12 @@ window.addEventListener('load', function() {
         constructor(width, height) {
             this.width = width;
             this.height = height;
+            this.groundMargin = 50;
             this.player = new Player(this); 
             this.input = new InputHandler();
         }
-        update() {
-            this.player.update(this.input.keys);
+        update(deltaTime) {
+            this.player.update(this.input.keys, deltaTime);
         }
         draw(context) {
             this.player.draw(context);
@@ -25,13 +26,19 @@ window.addEventListener('load', function() {
     }
 
     const game = new Game(canvas.width, canvas.height);
+    let lastTime = 0;
 
-    function animate() {
+    function animate(timeStamp) {
+        // timeStamp is the current time
+        // lastTime is the time of the previous frame
+        // deltaTime is the time between the current and previous frame
+        const deltaTime = timeStamp - lastTime;
+        lastTime = timeStamp;
         ctx.clearRect(0, 0, canvas.width, canvas.height);
-        game.update();
+        game.update(deltaTime);
         game.draw(ctx);
         requestAnimationFrame(animate);
     }
-    animate();
+    animate(0);
 });
 
--- player.js
+++ player.js
@@ -1,3 +1,5 @@
+import { Falling, Jumping, Running, Sitting } from "./playerStates.js";
+
 export class Player {
     constructor(game) {
         this.game = game;
@@ -6,16 +8,40 @@ export class Player {
         this.height = 91.3;
         // position of the player on the canvas (these coordinates are starting from the top left corner of the canvas)
         this.x = 100;
-        this.y = this.game.height - (this.height * 2);
+        this.y = this.game.height - (this.height * 2) - this.game.groundMargin;
         this.image = player;
+        
+        // speed is the speed of the player in the x-direction
         this.speed = 0;
         this.maxSpeed = 5;
+
+        // velocity is the speed of the player in the y-direction
         this.vy = 0;
         this.weight = 1;
-    }
-    update(input) {
 
+        // frameX and frameY are the coordinates of the player image on the sprite sheet
+        this.frameX = 0;
+        this.frameY = 0;
+
+        // maxFrame is the maximum number of frames in the sprite sheet
+        this.maxFrame = 5;
+
+        // fps is the frames per second
+        this.fps = 20;
+        this.frameInterval = 1000 / this.fps;
+        this.frameTimer = 0;
+
+        // states are the different states the player can be in
+        this.states = [new Sitting(this), new Running(this), new Jumping(this), new Falling(this)];
+        this.currentState = this.states[0];
+        this.currentState.enter();
+    }
+    update(input, deltaTime) {
+        // get input from the game class and pass it to the player state class
+        this.currentState.handleInput(input);
+        
         // controls horizontal (x-direction) movement
+        this.x += this.speed;
         if (input.includes('ArrowLeft')) {
             this.speed = -this.maxSpeed;
         }
@@ -25,29 +51,17 @@ export class Player {
         else {
             this.speed = 0;
         }
-        this.x += this.speed;
         // condition to prevent the player from moving out of the canvas
         if (this.x < 0) {
             this.x = 0;
         }
-        else if (this.x > this.game.width - (this.width * 2)) {
+
+        if (this.x > this.game.width - (this.width * 2)) {
             this.x = this.game.width - (this.width * 2);
         }
 
         // controls vertical (y-direction) movement
-        if (input.includes('ArrowUp') && this.onGround()) {
-            this.vy -= 30;
-        }
-        else if (input.includes('ArrowDown')) {
-            this.vy += 30;
-        }
-
-        
-        if(this.y < 0) {
-            this.y = 0;
-        } else {
-            this.y += this.vy;
-        }
+        this.y += this.vy;
 
         // when the player is not on the ground, the gravity is applied
         if (!this.onGround()) {
@@ -56,6 +70,20 @@ export class Player {
         else {
             this.vy = 0;
         }
+
+        // sprite animation
+        if (this.frameTimer > this.frameInterval) {
+            this.frameTimer = 0;
+            if (this.frameX < this.maxFrame) {
+                this.frameX++;
+            }
+            else {
+                this.frameX = 0;
+            }
+        }
+        else {
+            this.frameTimer += deltaTime;
+        }
     }
     draw(context) {
         // 1st parameter is the image
@@ -63,11 +91,14 @@ export class Player {
         // 4th, 5th parameter is the width and height of the image on the sprite sheet
         // 6th, 7th parameter is the position of the image on the canvas
         // 8th, 9th parameter is the width and height of the image on the canvas
-        context.drawImage(this.image, 0, 0, this.width, this.height, this.x, this.y, this.width * 2, this.height * 2);
+        context.drawImage(this.image, this.frameX * this.width, this.frameY * this.height, this.width, this.height, this.x, this.y, this.width * 2, this.height * 2);
     }
     onGround() {
-        return this.y >= this.game.height - (this.height * 2);
+        return this.y >= (this.game.height - (this.height * 2) - this.game.groundMargin);
     }
-        
+    setState(state) {
+        this.currentState = this.states[state];
+        this.currentState.enter();
+    }
 }
 
